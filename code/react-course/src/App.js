be046faa3c47d51471b458795d10660a2acd5045
Bri <brittanymorris@MacBook-Pro.local>
2021-09-14 08:13:38 -0600

    Started a new but simular practice project.

--- code/react-course/src/App.js
+++ code/react-course/src/App.js
@@ -1,12 +1,6 @@
-import Todo from './components/Todo';
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React'/>
-      <Todo text='Use React'/>
-      <Todo text='Master React'/>
     </div>
   );
 }
